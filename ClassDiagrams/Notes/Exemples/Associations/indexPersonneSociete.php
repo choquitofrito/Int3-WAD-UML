a4f9fe05e7a1a98c738af43ca6efdee9748e2084
choquitofrito <zyriab@gmail.com>
2020-12-14 14:23:03 +0100

    exemples classes I

--- ClassDiagrams/Notes/Exemples/Associations/indexPersonneSociete.php
+++ ClassDiagrams/Notes/Exemples/Associations/indexPersonneSociete.php
@@ -10,6 +10,12 @@
     <?php
     include_once "./Personne.class.php";
     include_once "./Societe.class.php";
+
+    $p = new Personne("Lola");
+    $s = new Societe ("Lola & CO");
+    echo "<br>Nom de la societé: ".$s->getNom();
+    echo "<br>Nom de l'employé: ".$p->getNom();
+
     
     ?>
 </body>
