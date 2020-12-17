59c9a018f7c05f008e1e2c9718af28237c314998
choquitofrito <zyriab@gmail.com>
2020-12-17 09:05:33 +0100

    association classe association

---
+++ ClassDiagrams/Notes/Exemples/AssociationClasseAssociation/Personne.class.php
@@ -0,0 +1,51 @@
+<?php
+
+class Personne {
+    public $nom;
+    public $adresse;
+
+    public function __construct($nom, $adresse){
+        $this->nom = $nom;
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adresse
+     */ 
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set the value of adresse
+     *
+     * @return  self
+     */ 
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+}
