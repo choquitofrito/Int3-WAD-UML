f0771f31423816718a370e35ea590412180bad30
choquitofrito <zyriab@gmail.com>
2020-12-17 10:54:33 +0100

    assocs ok

--- ClassDiagrams/Notes/Exemples/Associations (plusieurs à plusieurs)/2.Plusieurs à plusieurs (avec classe association - attributs)/Contrat.class.php
+++ ClassDiagrams/Notes/Exemples/Associations (plusieurs à plusieurs)/2.Plusieurs à plusieurs (avec classe association - attributs)/Contrat.class.php
@@ -90,4 +90,24 @@ class Contrat {
 
         return $this;
     }
+
+    /**
+     * Get the value of salaire
+     */ 
+    public function getSalaire()
+    {
+        return $this->salaire;
+    }
+
+    /**
+     * Set the value of salaire
+     *
+     * @return  self
+     */ 
+    public function setSalaire($salaire)
+    {
+        $this->salaire = $salaire;
+
+        return $this;
+    }
 }
